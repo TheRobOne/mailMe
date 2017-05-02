8e3285f9dfdee5836a08d2230ff3e562dd703671
Wojtek Sabat <wojsabat@gmail.com>
2017-05-02 09:53:35 +0200

    added database configuration

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// connect to database
+mongoose.connect(config.database);
+
+// on database connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+
+// on database error
+mongoose.connection.on('error', (err) => {
+    console.log('database error: ' + err);
+});
 
 // app init
 const app = express();
