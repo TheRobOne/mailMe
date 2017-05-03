5af394510b2b0c374ab712a171a47269281144c2
Wojtek Sabat <wojsabat@gmail.com>
2017-05-03 11:32:40 +0200

    implemented profile component

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -16,21 +16,35 @@ export class AuthService {
       .map(res => res.json());
   }
 
-  authenticateUser(user){
+  authenticateUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/authenticate', user, { headers: headers })
       .map(res => res.json());
   }
 
-  storeUserData(token, user){
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken)
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', { headers: headers })
+      .map(res => res.json());
+  }
+
+  storeUserData(token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user = user;
   }
 
-  logout(){
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  logout() {
     this.authToken = null;
     this.user = null;
     localStorage.clear();
