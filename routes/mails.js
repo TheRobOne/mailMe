001ac7b3625daef00ea89c920ad0b68bbd4d2a8a
Michal Piatek <michalpiatekck@gmail.com>
2017-05-15 17:16:30 +0200

    Finish Frontend

---
+++ routes/mails.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const Mail = require('../models/mails');
+
+
+
+// Send
+router.get('/send', (req, res, next) => {
+
+});
+
+module.exports = router;
